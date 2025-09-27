1e1b506d7a0a9ce535e5a0daa8595c6ef8878b28
Leissom <leisoom.gg@gmail.com>
2025-09-26 21:50:16 -0700

    basic player stats route created

--- donut-smp-compendium.api/main.py
+++ donut-smp-compendium.api/main.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI,HTTPException
 import requests
 from dotenv import load_dotenv
 import os
@@ -9,10 +8,29 @@ app = FastAPI()
 
 api_key = os.getenv("API_KEY")
 
-@app.get("/", )
-async def root():
+@app.get("/player/{player_name}", )
+async def display_player_stats(player_name : str):
 
-    player = requests.get("https://api.donutsmp.net/v1/stats/leisoom",headers={'Authorization': f"Bearer {api_key}"});
-    player_json = player.json()['result']
+        data = requests.get(f"https://api.donutsmp.net/v1/stats/{player_name}",headers={'Authorization': f"Bearer {api_key}"}).json()
+        
+        if(data["status"] == 500):
+           raise HTTPException(status_code=404, detail="player not found")
+        
+        player_json = data['result']
+        platform = "bedrock" if player_name.startswith(".") else "java"
 
-    return {'shards' : player_json['deaths']}
+        player_response = {};
+        player_response["name"] = player_name
+        player_response["platform"] = platform
+        player_response["broken_blocks"] = player_json['broken_blocks']
+        player_response["deaths"] = player_json['deaths']
+        player_response["kills"] = player_json['kills']
+        player_response["mobs_killed"] = player_json['mobs_killed']
+        player_response["money"] = player_json['money']
+        player_response["money_made_from_sell"] = player_json['money_made_from_sell']
+        player_response["money_spent_on_shop"] = player_json['money_spent_on_shop']
+        player_response["placed_blocks"] = player_json['placed_blocks']
+        player_response["playtime"] = player_json['playtime']
+        player_response["shards"] = player_json['shards']
+
+        return player_response
