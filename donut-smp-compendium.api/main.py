c31ba29a7335eb7f950451a973b03db435d84640
Leissom <leisoom.gg@gmail.com>
2025-09-26 22:12:10 -0700

    add player model for returning player data

--- donut-smp-compendium.api/main.py
+++ donut-smp-compendium.api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI,HTTPException
+from pydantic import BaseModel
 import requests
 from dotenv import load_dotenv
 import os
@@ -8,29 +9,43 @@ app = FastAPI()
 
 api_key = os.getenv("API_KEY")
 
-@app.get("/player/{player_name}", )
-async def display_player_stats(player_name : str):
+class Player(BaseModel):
+    name: str
+    platform: str
+    broken_blocks: int
+    deaths: int
+    kills: int
+    mobs_killed: int
+    money: int
+    money_made_from_sell: float
+    money_spent_on_shop: int
+    placed_blocks: int
+    playtime: int
+    shards: int
 
-        data = requests.get(f"https://api.donutsmp.net/v1/stats/{player_name}",headers={'Authorization': f"Bearer {api_key}"}).json()
-        
-        if(data["status"] == 500):
-           raise HTTPException(status_code=404, detail="player not found")
-        
-        player_json = data['result']
-        platform = "bedrock" if player_name.startswith(".") else "java"
+@app.get("/player/{player_name}", response_model=Player)
+async def display_player_stats(player_name : str,):
+    data = requests.get(f"https://api.donutsmp.net/v1/stats/{player_name}",headers={'Authorization': f"Bearer {api_key}"}).json()
+    
+    if(data["status"] == 500):
+        raise HTTPException(status_code=404, detail="player not found")
+    
+    player_json = data['result']
+    platform = "bedrock" if player_name.startswith(".") else "java"
 
-        player_response = {};
-        player_response["name"] = player_name
-        player_response["platform"] = platform
-        player_response["broken_blocks"] = player_json['broken_blocks']
-        player_response["deaths"] = player_json['deaths']
-        player_response["kills"] = player_json['kills']
-        player_response["mobs_killed"] = player_json['mobs_killed']
-        player_response["money"] = player_json['money']
-        player_response["money_made_from_sell"] = player_json['money_made_from_sell']
-        player_response["money_spent_on_shop"] = player_json['money_spent_on_shop']
-        player_response["placed_blocks"] = player_json['placed_blocks']
-        player_response["playtime"] = player_json['playtime']
-        player_response["shards"] = player_json['shards']
+    player_response = Player(
+            name = player_name, 
+            platform=platform,
+            broken_blocks= player_json['broken_blocks'],
+            deaths=player_json['deaths'],
+            kills=player_json['kills'],
+            mobs_killed=player_json['mobs_killed'],
+            money= player_json['money'],
+            money_made_from_sell= player_json['money_made_from_sell'],
+            money_spent_on_shop= player_json['money_spent_on_shop'],
+            placed_blocks=player_json['placed_blocks'],
+            playtime=player_json['playtime'],
+            shards=player_json['shards']
+            )
 
-        return player_response
+    return player_response
